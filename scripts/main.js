725056f5feb3f4c55f6494bbd5dd7b34a2c49a4c
Basel Nsralla <basns594@student.liu.se>
2020-01-29 16:07:55 +0100

    Create blue objects

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,12 @@
 import FlowchartNode from "../flowchart-node";
-
+const uuidv1 = require('uuid/v1');
 let markedObject = {};
 
 function main() {
+    let objectIds = [];
     function createNewObject(){
         // Funktion som kallas då knappen "skapa nytt objekt trycks"
-        console.log("NY");
-        const flowObj = new FlowchartNode();
-        flowObj.print();
+
 
         if (document.getElementById("element-picker").style.visibility == "hidden"){
             document.getElementById("element-picker").style.visibility = "visible";
@@ -15,10 +14,16 @@ function main() {
         else {
             document.getElementById("element-picker").style.visibility = "hidden";
         }
-    
+        const id = uuidv1();
+        objectIds.push(id);
+        const flowObj = new FlowchartNode(id);
+        const workspaceRoot = document.querySelector('#workspace-root');
+        workspaceRoot.appendChild(flowObj.render());
+        flowObj.print();
     }
 
     document.querySelector("#newObject").addEventListener("click", createNewObject)
+
 }
 
 (function() {
