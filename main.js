3c74e33f1351ee5deb8232e7cf165e2e0124090d
Basel Nsralla <basns594@student.liu.se>
2020-01-29 15:27:54 +0100

    webpack working, some testobjects were created to test webpack

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-function createNewObject(){
-    // Funktion som kallas då knappen "skapa nytt objekt trycks"
-    console.log("NY");
+import FlowchartNode from "./flowchart-node";
+
+
+function main() {
+    function createNewObject(){
+        // Funktion som kallas då knappen "skapa nytt objekt trycks"
+        console.log("NY");
+        const flowObj = new FlowchartNode();
+        flowObj.print();
+    }
+
+    document.querySelector("#newObject").addEventListener("click", createNewObject)
 }
+
+(function() {
+   main();
+})();
