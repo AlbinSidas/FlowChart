cf014b421c361dcbe825ea830249571899eaf837
Basel Nsralla <basns594@student.liu.se>
2020-02-06 16:20:41 +0100

    html loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,14 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
+      ],
     }
 
 }
