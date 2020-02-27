3b2c70e86e1accae810e42bb5692eb074fed484e
Basel <basel.obeid9661@gmail.com>
2020-02-27 13:14:15 +0100

    validation for data comming from API calls

--- server/app.js
+++ server/app.js
@@ -7,14 +7,19 @@ const bodyParser   = require('body-parser')
 const fs           = require('fs');
 const serverConfig = config.server
 const dbConfig     = config.db 
+const MongoHandler = mongo.MongoHanlder;
+const Schema       = require('./schema.js')
 
 async function main() {
 
-    const app     = express()
-    const url     = `mongodb://${dbConfig.ip_addr}:${dbConfig.port}`;
-    const dbName  = dbConfig.db_name;
-    await mongo.setup(url, dbName) //när du awaitar så kallar du på din promies then med resten av koden, kolla <Generators>
-    app.use(bodyParser.json())
+    const app          = express()
+    const url          = `mongodb://${dbConfig.ip_addr}:${dbConfig.port}`;
+    const dbName       = dbConfig.db_name;
+    const db           = await mongo.setup(url, dbName); //när du awaitar så kallar du på din promies then med resten av koden, kolla <Generators>
+    const mongoHandler = MongoHandler(db);
+    
+    app.use(bodyParser.json());
+    
     app.use(cors({
         "credentials": true,
         "origin": ["http://localhost:9000"],
@@ -25,7 +30,7 @@ async function main() {
     console.log("Back to main generated function")
     app.get('/', (req, res) => res.json({'apa':'Hello World!'}))
 
-    app.put('/save', function (req, res){
+    app.put('/saved', function (req, res){
         fs.writeFile("./saved/"+req.body.filename+".json",JSON.stringify(req.body.data),function (err) {
             if (err) throw err;
             console.log('File is created successfully.');
@@ -38,15 +43,28 @@ async function main() {
             if (err) throw err;
             res.send(data);
           }); 
-    })
+    });
     
     app.get('/loadfilenames', function (req, res){
         fs.readdir("./saved/",function (err, files) {
             if (err) throw err;
             res.send(files);
           }); 
-    })
+    });
 
+    app.post('/save', async (req, res) => {
+        const data = req.body;
+        try {
+             await Schema.validate(data, Schema.jsonSchemas.funcDefSchema);
+        } catch (InvalidTypeError) {
+            console.log(InvalidTypeError)
+            res.status(400);
+            res.send(InvalidTypeError.message);
+        }
+        mongoHandler.saveFunctionDef(data)
+        res.status(200)
+        res.end("express");
+    });
     app.listen(serverConfig.port, () => console.log(`Foran Flowchart server listening on port ${serverConfig.port}!`))
 }
 
--- server/mongo.js
+++ server/mongo.js
@@ -3,20 +3,51 @@ const assert      = require('assert');
 
 
 
+/*
+    template:
+        function description,
+        function name,
+        command
+*/
+
+
 async function setup(url, dbName) { // kan abstraheras om man vill
     return await new Promise((accept, reject) => {
-        MongoClient.connect(url, { useUnifiedTopology: true } , function(err, client) {
+        MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true } , function(err, client) {
             assert.equal(null, err);
             console.log("Connected successfully to database");
             const db = client.db(dbName); 
-            client.close();
-            accept()
+            //client.close();
+            accept(db)
         });
     })
 }
 
+/*
+    data: 
+        id; Object Int
+        
+*/
+
+const MongoHanlder = (db) => {
+
+    return {
+        saveFunctionDef: async (data) =>  {
+            const collection = db.collection('function_definition');
+            collection.insertOne(data, (err, result)=> {
+                console.log(err);
+                console.log(result.ops);
+            });
+            console.log("RES ");
+        }
+    }
+}
+
+
+
 
 
 module.exports = {
-    setup
+    setup,
+    MongoHanlder
 } 
