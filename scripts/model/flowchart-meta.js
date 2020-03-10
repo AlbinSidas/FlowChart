a46729b852319a50436c250ea26849835c4aacac
Basel Nsralla <basns594@student.liu.se>
2020-03-10 08:51:18 +0100

    quick fixes

--- scripts/model/flowchart-meta.js
+++
@@ -1,9 +0,0 @@
-
-
-class FlowchartMeta {
-    constructor(name, nodes) {
-
-    }
-
-
-}
--- scripts/model/function-variable.js
+++ scripts/model/function-variable.js
@@ -1,10 +1,10 @@
 
 class FunctionVariable {
     constructor(name, type, value, extra ={}){
-        this.name = name;    // potatis
-        this.type = type;    // output
-        this.value = value;  // han fyller
-        this.extra = extra;  // saker som den här typen har unikit
+        this.name = name;    // exempel potatis
+        this.type = type;    // exempel output
+        this.value = value;  // sake Martin fyller
+        this.extra = extra;  // saker som gör den här typen har unikit
     }
 }
 export default FunctionVariable;
