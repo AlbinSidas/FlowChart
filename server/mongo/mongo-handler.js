0214c0873794b9470dc1332a583d434593c60fee
Basel Nsralla <basns594@student.liu.se>
2020-03-31 12:00:53 +0200

    funcdef versioning working

--- server/mongo/mongo-handler.js
+++ server/mongo/mongo-handler.js
@@ -14,22 +14,68 @@ class MongoHandler {
     }
 
     async save (data)  {
+        const latestVersionNumber = 1;
         const insertOne  = _promisify((...args) => { this.collection.insertOne(...args) });
-        const result     =  await insertOne(data).then(a  => a)
+        const result     =  await insertOne({latestVersionNumber: latestVersionNumber, versions: [
+            {
+                ...data,
+                version: latestVersionNumber
+            }
+        ]}).then(a  => a)
                                                  .catch(e => console.log("SAVE ERRROOORO", e))
         return result.ops
     }
     
     async getById (id) {
-        const collectionFunc  = _promisify((...args) => { this.collection.findOne(...args) });
-        const result          =  await collectionFunc({_id: ObjectID(id)}).then(a  => a)
-                                                                          .catch(e => console.log(e))
-        return result
+        const findById   = _promisify((...args) => { this.collection.aggregate(...args) });
+        const result     =  await findById([
+            {
+                $match: {_id:  ObjectID(id) }
+            }, 
+            {
+                $project: 
+                {
+                    latestVersionNumber: 1,
+                    last: { $arrayElemAt: ["$versions", -1]} 
+                }
+            }
+        
+        ]).then(a  => a)
+          .catch(e => console.log(e))
+        const allEntries = await result.toArray();
+        return allEntries[0]
     }
 
     async getAll() {
         const data = this.collection.find();
         return await data.toArray();
+    }
+
+    async addVersion(data) {
+        const update = _promisify((...args) => { this.collection.update(...args) });
+        const oldEntry       = await this.getById(data._id);
+        const updatedVersion = oldEntry.latestVersionNumber + 1;
+        const versionEntry   = { ...data.content, version: updatedVersion }
+
+        const result = await update(
+            // ID
+            {_id: ObjectID(data._id)},
+            {
+                $set: { "latestVersionNumber": updatedVersion },
+                $push: {
+                    versions: versionEntry
+                }
+            }
+        ).catch(err => { console.log(err); 
+                         return null 
+                       });
+
+        return {_id: data._id, ...versionEntry};
+    }
+
+
+    async getVersionSnpashot() {
+        
     }
 
 }
