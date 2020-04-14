f0a7ed080ba123249dadf68e78b86dcd37c68576
vidsi889 <vidsi889@student.liu.se>
2020-04-14 11:01:18 +0200

    started on ifnodes

--- scripts/node/conditional-node.js
+++ scripts/node/conditional-node.js
@@ -1,8 +1,65 @@
 import FlowchartNode from "../flowchart-node";
+import ifstyle from "../Styles/style.css";
 
 class ConditionalNode extends FlowchartNode {
     //vid input 0 kör if-delen
-    //input annat än 0 kör else-delen 
+    //input annat än 0 kör else-delen
+    constructor(id) {
+        super()
+        this.setHtml('<div></div>')
+
+        //functions
+        
+        //ui
+        this.posX    = 100;
+        this.posY    = 100;
+        this.height  = 250;
+        this.offsetX = 0;
+        this.offsetY = 0;
+        this.idRef   = "";
+        this._connectorUpdaters = {}
+        //flow
+        this.id    = id;
+        this._name = "";
+
+        this.input  = new NodeIO(this, "box-input");
+        this.outputIf = new NodeIO(this, "box-output");
+        this.outputElse = new NodeIO(this, "box-output");
+
+        this.element.classList.add(style.if_node);
+
+
+    }
+
+    didAttach()
+
+    copyOther()
+
+    registerConnectorUpdater()
+
+    removeConnectorUpdater()
+
+    unregisterConnectorUpdater()
+
+    getName()
+
+    setName()
+
+    render()
+
+    elementDrag()
+
+    dragOthers()
+
+    closeDragElement()
+
+    mouseDown()
+
+    onClick()
+
+
+
+     
     getMetaInfo() {
         return 
             new NodeMetaInfo(
@@ -13,7 +70,7 @@ class ConditionalNode extends FlowchartNode {
                 this.id, 
                 this.input.connections, 
                 this.output.connections, 
-                this.functionVariables); //behövs detta?
+                this.functionVariables); //behövs detta, troligen inte?
     }
 
 
