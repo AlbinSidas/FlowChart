4ad900022e84bad04793004bdd7058663b793829
mac <mac>
2020-04-29 14:00:07 +0200

    Logger done

--- server/util/logger.js
+++ server/util/logger.js
@@ -1,53 +1,76 @@
+const chalk = require('chalk');
 
-const mode = process.env.MODE;
+const mode = process.env.NODE_ENV;
 
 const ModeToNum = {
     prod:  0,
     debug: 1,
     dev  : 2
-} // function som retunerar -1 ? 
+} // function som retunerar -1 ?
 
 const _translate = (mode, logLevel, exclusive = false) => {
     const modeNum = ModeToNum[mode];
-    if(!modeNum) return false;    
-    return (!exclusive && modeNum >= logLevel) || (exclusive && modeNum == logLevel)
+    const levelNum = ModeToNum[logLevel]
+    if(isNaN(modeNum) || isNaN(levelNum)) return false;    
+    return (!exclusive && modeNum >= levelNum) || (exclusive && modeNum == levelNum)
 } 
 
 // =================================
 
-function debugLog(...args) {
-    console.log(chalk.blue.bgYellow.bold('DEBUG:'), ...args);
+function debug(...args) {
+    console.log(chalk.black.bgMagenta.bold('DEBUG:'), ...args);
 }
 
-function prodLog(...args) {
-    console.log(chalk.blue.bgYellow.bold('PROD:'), ...args);
+function info(...args) {
+    console.log(chalk.black.bgBlue.bold('INFO:'), ...args);
 }
 
-function devLog(...args) {
-    console.log(chalk.blue.bgYellow.bold('DEV:'), ...args);
+function log(...args) {
+    console.log(chalk.white.bgCyan.bold('LOG:'), ...args);
+}
+
+function warn(...args) {
+    console.log(chalk.black.bgYellow.bold('WARN:'), ...args);
+}
+
+function error(...args) {
+    console.log(chalk.black.bgRed.bold('ERROR:'), ...args);
 }
 
 //========================================
 
 function debug_activate () {
-    if(_translate(mode), 'debug') { return debugLog; }
+    if(_translate(mode, 'debug')) { return debug; }
     return () => {};
 }
 
-function prod_activate(...args) {
-    if(_translate(mode), 'prod')  { return prodLog; }
+function info_activate(...args) {
+    if(_translate(mode, 'prod'))  { return info; }
     return () => {};
 }
 
-function dev_activate(...args) {
-    if(_translate(mode), 'dev')   { return devLog; }
+function log_activate(...args) {
+    if(_translate(mode, 'dev'))   { return log; }
     return () => {};
 }
 
-export default {
-    logDebug: debug_activate(),
-    logProd:  prod_activate(),
-    log:      dev_activate(),
+function warn_activate(...args) {
+    if(_translate(mode,'debug')) { return warn; }
+    return () => {};
 }
 
+function error_activate(...args) {
+    if(_translate(mode,'prod'))  { return  error; }
+    return () => {};
+}
+
+exported = {
+    debug: debug_activate(),
+    info:  info_activate(),
+    log:   log_activate(),
+    warn:  warn_activate(),
+    error: error_activate()
+};
+
+module.exports = exported;
 
